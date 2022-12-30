a36682523c4e853bfa7a8b9b3f802bb9fe5c70f5
ArkQuark <74979909+ArkQuark@users.noreply.github.com>
2022-12-30 14:48:53 +0300

    Playermotd

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -550,6 +550,13 @@ class Server{
 	public function getMotd(){
 		return $this->getConfigString("motd", "Minecraft: PE Server");
 	}
+	
+	/**
+	 * @return string
+	 */
+	public function getPlayerMotd(){
+		return $this->getConfigString("player-motd", "Welcome @player to this server!");
+	}
 
 	/**
 	 * @return \ClassLoader
@@ -1579,6 +1586,7 @@ class Server{
 		$this->logger->info("Loading server properties...");
 		$this->properties = new Config($this->dataPath . "server.properties", Config::PROPERTIES, [
 			"motd" => "Minecraft: PE Server",
+			"player-motd" => "Welcome @player to this server!",
 			"server-port" => 19132,
 			"white-list" => \false,
 			"announce-player-achievements" => \true,
