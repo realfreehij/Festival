94109badc8a1b42240f956b15a6e72f8aa3b6e1b
GameHerobrine <GameHerobrine@users.noreply.github.com>
2023-12-10 05:33:45 +0300

    wall signs fix

--- src/pocketmine/block/WallSign.php
+++ src/pocketmine/block/WallSign.php
@@ -23,7 +23,12 @@ namespace pocketmine\block;
 
 
 class WallSign extends SignPost{
-
+	static $meta2side = [
+		2 => 3,
+		3 => 2,
+		4 => 5,
+		5 => 4
+	];
 	protected $id = self::WALL_SIGN;
 
 	public function getName(){
@@ -31,6 +36,11 @@ class WallSign extends SignPost{
 	}
 
 	public function onUpdate($type){
-		return \false;
+		$attached = $this->getSide(self::$meta2side[$this->meta]);
+		if(!$attached->isSolid() && ($attached->getId() !== Block::WALL_SIGN && $attached->getId() !== Block::SIGN_POST)){
+			$this->getLevel()->setBlock($this, new Air(), \true, \true);
+		}
+		
+		return $type;
 	}
 }
