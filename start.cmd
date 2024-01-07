e1a56c41985070c0f34f98f685277b1fe230d446
FreeHij <realfreehij@gmail.com>
2024-01-08 01:57:40 +0300

    start.cmd fix

--- start.cmd
+++ start.cmd
@@ -2,9 +2,9 @@
 TITLE Festival server software for Minecraft: Pocket Edition
 cd /d %~dp0
 
-if exist bin\php8\php.exe (
+if exist bin\php\php.exe (
 	set PHPRC=""
-	set PHP_BINARY=bin\php8\php.exe
+	set PHP_BINARY=bin\php\php.exe
 ) else (
 	set PHP_BINARY=php
 )
@@ -27,7 +27,7 @@ REM ) else (
 	if exist bin\mintty.exe (
 		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "Festival" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
 	) else (
-		%PHP_BINARY% -c bin\php8 %POCKETMINE_FILE% %*
+		%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
 		pause
 	)
 REM )
